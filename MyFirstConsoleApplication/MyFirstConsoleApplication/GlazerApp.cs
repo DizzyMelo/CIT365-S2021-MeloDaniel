2088053afbe8985cc3e6765adc1d6b426a28807b
Daniel Melo <daniel.melo42@outlook.com>
2021-04-20 17:48:05 -0300

    W01 Assignment

---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/GlazerApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstConsoleApplication
+{
+    class GlazerApp
+    {
+        public static void RunExample() 
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            
+            Console.WriteLine("Enter the width:");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.WriteLine("Enter the height:");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is {0} feet", woodLength);
+
+            Console.WriteLine("The area of the glass is {0} square metres", glassArea);
+        }
+    }
+}
---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyFirstConsoleApplication
+{
+    class Program
+    {
+        static private void GetUserNameAndLocation()
+        {
+            Person person = new Person();
+            Console.WriteLine("What is your name?");
+            person.name = Console.ReadLine();
+            
+            Console.WriteLine("Hi {0}! Where are you from?", person.name);
+            person.location = Console.ReadLine();
+            
+            Console.WriteLine("I have never been to {0}. I bet it is nice. Press any key to continue...", person.location);
+            Console.ReadKey();
+
+        }
+
+        static private void ChristmasCountdown(DateTime date) 
+        {
+            DateTime christmasDate = new DateTime(2021, 12, 25);
+            int days = christmasDate.Subtract(date).Days;
+            Console.WriteLine("Today's date is: {0}", date.ToString("MM/dd/yyyy"));
+            Console.WriteLine("There are {0} days until Christmas!", days);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            //GetUserNameAndLocation();
+            //ChristmasCountdown(DateTime.Now);
+
+            GlazerApp.RunExample();
+        }
+        
+    }
+}
